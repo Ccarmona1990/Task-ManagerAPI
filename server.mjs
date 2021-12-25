304d05d97236251de72c6dfd336bebd3960c9369
Christopher Carmona <c.carmona0990@gmail.com>
2021-12-25 11:33:54 -0400

    update
    config vars added on heroku. morgan added

--- server.mjs
+++ server.mjs
@@ -3,24 +3,29 @@ import greetingRoute from "./routes/greeting.mjs"
 import taskRoute from './routes/tasks.mjs';
 import DBconnection from './db/connect.mjs';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 const URI = process.env.MONGO_URI;
 
 // form parse
 app.use(express.urlencoded({extended:false}))
 // json parse
-app.use(express.json()) 
+app.use(express.json())
+
 // routes
 app.use('/', greetingRoute)
 app.use('/api/v1/tasks', taskRoute);
 
+// http request logger
+app.use(morgan('tiny'))
+
 const start = async ()=>{
     try {
         await DBconnection(URI)
-        app.listen(port, ()=>console.log(`Server open on port: ${port}`))
+        app.listen(port)
     } catch (err) {
         console.log(err);
     }
